dfc141778e105caf799d0ba8eafbbe95646bb70a
Jazzlyn Aquino <katejazzlyn@gmail.com>
2025-04-16 17:20:27 +0800

    updated matcha

--- app/graphic-branding/page.tsx
+++ app/graphic-branding/page.tsx
@@ -105,10 +105,17 @@ export default function GraphicBrandingPage() {
 
         <div className="mb-16">
           <h3 className="text-2xl font-bold mb-8 mx-auto text-center">
-            Illustrated Paper Bag Design for a Matcha Brand
+           Illustrated Design Concepts for Matcha Mug and Paper Bag
           </h3>
           <Image
             src="/images/design/graphic-design/matcha-bag.png"
+            alt="ewan"
+            width={800}
+            height={800}
+            className="max-w-[650px] w-full h-full object-cover transition-transform group-hover:scale-105 duration-500 mx-auto rounded-xl"
+          />
+                    <Image
+            src="/images/design/graphic-design/matcha-mug.png"
             alt="ewan"
             width={800}
             height={800}
